f4ed7a9567ca0f121002a5b81650c25765bd7ac1
tungnguyenthanh888 <tungdev3010@gmail.com>
2026-05-12 16:53:22 +0700

    first commit

---
+++ medical-system-server/src/test/java/com/medical/medicalsystemserver/MedicalSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.medicalsystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

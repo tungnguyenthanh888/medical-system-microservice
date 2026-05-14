a4f840c7310b1d0a32ac312f889c79fb95c6c5df
tungnguyenthanh888 <tungdev3010@gmail.com>
2026-05-14 16:50:28 +0700

    Update refresh auto update config variables.

---
+++ patient-service/src/main/java/com/medical/patientservice/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.medical.patientservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+@RefreshScope
+public class WelcomeController {
+    @Value("${app.welcome}")
+    private String welcome;
+
+    @GetMapping
+    public String getWelcome()
+    {
+        return welcome;
+    }
+}
